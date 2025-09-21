c6017957e634183ebb32c9005d2a5037434da0bb
Magpys <1dangriffiths@gmail.com>
2025-09-21 09:51:58 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Clox Interpreter
+
+This is my implementation of the Clox interpreter as described in the book 'Crafting Interpreters'.
+
+## Usage
+
+Not a clue yet, I don't really know C very well.
---
+++ memory.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+
+#include "memory.h"
+
+void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
+    if (newSize == 0) {
+        free(pointer);
+        return NULL;
+    }
+
+    void* result = realloc(pointer, newSize);
+    // this means it has run out of memory...!!!
+    if (result == NULL) exit(1);
+    return result;
+}
