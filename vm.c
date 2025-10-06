7597349b0f6318edfb7d76b743d5a1df87dae270
Magpys <1dangriffiths@gmail.com>
2025-10-06 12:21:33 +0100

    Up to 16

--- vm.c
+++ vm.c
@@ -37,6 +37,16 @@ Value pop() {
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+    // note: this do while loop here is used so that we can safely group a bunch of statements that might not
+    // otherwise be safely contained in a macro function.
+    // Also, the author notes how wild it feels to pass an operator into a function,
+    // perks of the C pre-processor treating everything as tokens!
+#define BINARY_OP(op) \
+    do { \
+        double b = pop(); \
+        double a = pop(); \
+        push(a op b); \
+    } while (false)
 
     for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -58,6 +68,11 @@ static InterpretResult run() {
                 printf("\n");
                 break;
             }
+            case OP_ADD: BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE: BINARY_OP(/); break;
+            case OP_NEGATE: push(-pop()); break;
             case OP_RETURN:
                 printValue(pop());
                 printf("\n");
@@ -67,6 +82,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
