79cdfd95c99b4f5f7c11eeeb9bbfa3cfc8a8b9a9
Magpys <1dangriffiths@gmail.com>
2025-10-30 08:53:13 +0000

    up to chapter 19

--- chunk.h
+++ chunk.h
@@ -9,6 +9,13 @@ typedef enum {
     OP_NIL,
     OP_TRUE,
     OP_FALSE,
+    // note: We could (and perhaps should because it would be marginally faster) use specific OpCode instructions for
+    // OP_GREATER_EQUAL, OP_LESS_EQUAL and OP_NOT_EQUAL - but we can build these out of a mixture of the others.
+    // For example a <= b is equivalent to !(a > b), therefore we can build OP_LESS_EQUAL out of OP_GREATER followed by OP_NOT.
+    // (most of the time - there are actually some edge cases for which that may not be true around NaN but what the hell).
+    OP_EQUAL,
+    OP_GREATER,
+    OP_LESS,
     OP_ADD,
     OP_SUBTRACT,
     OP_MULTIPLY,
