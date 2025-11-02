797a8bf96c5ba9c45a4aa653ff63750f6d08dc66
Magpys <1dangriffiths@gmail.com>
2025-11-02 09:45:43 +0000

    implemented strings, up to chapter 19 challenges

--- compiler.h
+++ compiler.h
@@ -1,6 +1,8 @@
 #ifndef CLOX_COMPILER_H
 #define CLOX_COMPILER_H
+
 #include "chunk.h"
+#include "object.h"
 
 bool compile(const char* source, Chunk* chunk);
 
--- memory.c
+++ memory.c
@@ -2,6 +2,9 @@
 
 #include "memory.h"
 
+#include "value.h"
+#include "vm.h"
+
 void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     if (newSize == 0) {
         free(pointer);
@@ -12,4 +15,24 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
     // this means it has run out of memory...!!!
     if (result == NULL) exit(1);
     return result;
+}
+
+static void freeObject(Obj* object) {
+    switch (object->type) {
+        case OBJ_STRING: {
+            ObjString* string = (ObjString*)object;
+            FREE_ARRAY(char, string->chars, string->length + 1);
+            FREE(ObjString, object);
+            break;
+        }
+    }
+}
+
+void freeObjects() {
+    Obj* object = vm.objects;
+    while (object != NULL) {
+        Obj* next = object->next;
+        freeObject(object);
+        object = next;
+    }
 }
