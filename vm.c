9caff8ce5596b1dfa8a2ed9eaf9af0deb22b896f
Magpys <1dangriffiths@gmail.com>
2025-12-07 19:44:58 +0000

    up to chapter 21

--- vm.c
+++ vm.c
@@ -39,8 +39,10 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
 }
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
