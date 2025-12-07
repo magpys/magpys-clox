67ce6fce1bcfa1cc64ccbd1bfee69d61eb9a9ffa
Magpys <1dangriffiths@gmail.com>
2025-12-07 20:19:09 +0000

    up to 21.2. Added support for statements, print and expression statements.

--- chunk.h
+++ chunk.h
@@ -9,6 +9,7 @@ typedef enum {
     OP_NIL,
     OP_TRUE,
     OP_FALSE,
+    OP_POP,
     // note: We could (and perhaps should because it would be marginally faster) use specific OpCode instructions for
     // OP_GREATER_EQUAL, OP_LESS_EQUAL and OP_NOT_EQUAL - but we can build these out of a mixture of the others.
     // For example a <= b is equivalent to !(a > b), therefore we can build OP_LESS_EQUAL out of OP_GREATER followed by OP_NOT.
@@ -22,6 +23,7 @@ typedef enum {
     OP_DIVIDE,
     OP_NOT,
     OP_NEGATE,
+    OP_PRINT,
     OP_RETURN
 } OpCode;
 
