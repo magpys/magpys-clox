37580b1878debdd9f130eaf56fe70c01b307e7b9
Magpys <1dangriffiths@gmail.com>
2025-12-30 11:36:46 +0000

    added loops, up to ch 24

--- chunk.h
+++ chunk.h
@@ -30,6 +30,7 @@ typedef enum {
     OP_NEGATE,
     OP_JUMP,
     OP_JUMP_IF_FALSE,
+    OP_LOOP,
     OP_PRINT,
     OP_RETURN
 } OpCode;
--- vm.c
+++ vm.c
@@ -206,7 +206,12 @@ static InterpretResult run() {
             }
             case OP_JUMP_IF_FALSE: {
                 uint16_t offset = READ_SHORT();
-                if (isFalsey(peek(0))) vm.ip = offset;
+                if (isFalsey(peek(0))) vm.ip += offset;
+                break;
+            }
+            case OP_LOOP: {
+                uint16_t offset = READ_SHORT();
+                vm.ip -= offset;
                 break;
             }
             case OP_RETURN:
